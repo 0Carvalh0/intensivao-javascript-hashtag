a7004cf30586be3cab0c4cbd6f3f5bd9d34a4e33
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-02-03 12:24:44 -0300

    Função para escutar se o usuario enviou uma mensagem no backend

--- Projeto 04/servidor.js
+++ Projeto 04/servidor.js
@@ -5,8 +5,11 @@ const aplication = express();
 const servidorHttp = http.createServer(aplication);
 const io = require("socket.io")(servidorHttp);
 
-io.addListener("connection", () => {
+io.addListener("connection", (socket) => {
   console.log("Um usuario se conectou!");
+  socket.addListener("new message", (msg) => {
+    io.emit("new message", msg);
+  });
 });
 
 aplication.use(express.static("public"));
