fa4801cff2693f98d2b3e1705219d2e4a7a981b1
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-02-03 12:21:15 -0300

    Ação do botão de enviar

--- Projeto 04/public/script.js
+++ Projeto 04/public/script.js
@@ -1,1 +1,11 @@
+const btnEnviar = document.getElementById("enviar");
+const caixaTexto = document.getElementById("texto");
+
 const socket = io();
+
+btnEnviar.addEventListener("click", () => {
+  if (caixaTexto.value !== "") {
+    socket.emit("new message", caixaTexto.value);
+    caixaTexto.value = "";
+  }
+});
